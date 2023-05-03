a683100b2b4fb2fa3c636ab64a8fe97424b8e052
Thiago  Regis <93684026+Thiago-Regis@users.noreply.github.com>
2023-05-03 09:48:52 -0300

    Update esqueceuSenha.dart
    Criando o esqueceu a senha

---
+++ rpg_finder/lib/views/user/esqueceuSenha.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rpg_finder/views/user/login.dart';
+
+import '../../controllers/userController.dart';
+
+class EsqueceuSenha extends StatefulWidget {
+  const EsqueceuSenha({super.key});
+
+  @override
+  State<EsqueceuSenha> createState() => _EsqueceuSenhaState();
+}
+
+class _EsqueceuSenhaState extends State<EsqueceuSenha> {
+  String email = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Teste")),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLines: 1,
+                maxLength: 100,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.people_alt_rounded),
+                    labelText: "E-mail",
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    filled: true),
+                onChanged: (value) => email = value,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Campo de email obrigatório.";
+                  } /*else if (!EmailValidator.validate(value)) {
+                          return "Digite um email valido";
+                        }*/
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width + 10,
+                  child: ElevatedButton(
+                    onPressed: () => esqueceuSenha(email),
+                    child: const Text("Enviar"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
