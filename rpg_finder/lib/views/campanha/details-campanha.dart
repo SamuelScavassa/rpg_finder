9c4ad400b9e3f55ba90e06bd87e3846bacc1cc8e
Thiago  Regis <93684026+Thiago-Regis@users.noreply.github.com>
2023-05-03 09:47:04 -0300

    Add files via upload
    adicionando detalhes campanha

---
+++ rpg_finder/lib/views/campanha/details-campanha.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DetalhesCampanha extends StatefulWidget {
+  final QueryDocumentSnapshot<Map<String, dynamic>> campanha;
+
+  const DetalhesCampanha({Key? key, required this.campanha}) : super(key: key);
+
+  @override
+  State<DetalhesCampanha> createState() => _DetalhesCampanha();
+}
+
+class _DetalhesCampanha extends State<DetalhesCampanha> {
+  late final QueryDocumentSnapshot<Map<String, dynamic>> campanha;
+
+  @override
+  void initState() {
+    super.initState();
+    campanha = widget.campanha;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Detalhes da campanha ${campanha['nome'].toString().toLowerCase()}'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Nome da campanha: ${campanha['nome']}'),
+          Text('Descrição: ${campanha['descricao']}'),
+          Text('Jogadores: ${campanha['players']}'),
+          Text('Tags: ${campanha['tags'].join(", ")}'),
+          Text('Discord: ${campanha['discord']}'),
+          ElevatedButton(onPressed: null, child: Text("Juntar-se")),
+        ],
+      ),
+    );
+  }
+}
