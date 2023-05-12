7d6c2e26c87bc1e7b0577ae352212aa03188ecb1
Thiago  Regis <93684026+Thiago-Regis@users.noreply.github.com>
2023-05-12 14:44:45 -0300

    Update userController.dart

--- rpg_finder/lib/controllers/userController.dart
+++ rpg_finder/lib/controllers/userController.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:rpg_finder/views/user/esqueceuSenha.dart';
+
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
 FirebaseAuth auth = FirebaseAuth.instance;
